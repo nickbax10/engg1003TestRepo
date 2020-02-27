a4a1e44c5879d357c54451c662a3324c41f90121
Nicholas Bax <c3353685@uon.edu.au>
2020-02-27 12:40:51 +1100

    Initial Commit

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    float x;
+    float y;
+    printf("Enter a number: ");
+    scanf("%f", &x);
+    y = 2*x + (3 * 5);
+    printf("y: %f\n", y);
+    return 0;
+}
